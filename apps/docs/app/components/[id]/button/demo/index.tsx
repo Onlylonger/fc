895e9a0366393d6937c91882853544389adb0cc4
onlylonger <onlylonger@outlook.com>
2025-03-27 22:37:55 +0800

    U

--- apps/docs/app/components/[id]/button/demo/index.tsx
+++ apps/docs/app/components/[id]/button/demo/index.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { Button } from "@shilong/rc/components";
+import { MenuIcon } from "@shilong/rc/icons";
 import { LivePlayground } from "@/app/shared/LivePlaygournd";
 
-const scope = { Button };
+const scope = { Button, MenuIcon };
 import normalStr from "./normal-string.txt";
 
 export function Demo() {
